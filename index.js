a1b0a1281a2ba19c7c6e78a90b8e17b377096c7d
Jean Max Mezalon <+>
2021-11-01 13:56:54 -0400

    commit tree 565f6bc02fab7b21b5ecba4120598131b794652b

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    let obj = {
+        "firstName": array[0],
+        "familyName": array[1],
+        "title": array[2],
+        "payPerHour": array[3],
+        "timeInEvents": [],
+        "timeOutEvents": []
+    }
+
+    return obj
+}
+
+function createEmployeeRecords(arrOfarr) {
+
+    return arrOfarr.map(el => createEmployeeRecord(el))
+}
+
+function createTimeInEvent(dateStamp) {
+    let dates = dateStamp.split(" ")
+
+    this.timeInEvents.push({
+        "type": 'TimeIn',
+        "hour": parseInt(dates[1], 10),
+        "date": dates[0]
+    })
+
+    return this
+}
+
+
+function createTimeOutEvent(dateStamp) {
+    let dates = dateStamp.split(" ")
+
+    this.timeOutEvents.push({
+        "type": 'TimeOut',
+        "hour": parseInt(dates[1], 10),
+        "date": dates[0]
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let hourIn = this.timeInEvents.find(dates => dates.date === date);
+    let hourOut = this.timeOutEvents.find(dates => dates.date === date);    
+
+    let hoursWorked = hourOut.hour - hourIn.hour
+
+    return hoursWorked / 100
+}
+
+function wagesEarnedOnDate(date) {    
+    
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,5 +76,17 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArr, firstName) {
+    
+    return srcArr.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(employeeRecords) {
+    // console.log(employeeRecords);
+    // return
+    return employeeRecords.reduce(((acc, memo) => acc + memo.call(allWagesFor(), 0), this))
+
 }
 
